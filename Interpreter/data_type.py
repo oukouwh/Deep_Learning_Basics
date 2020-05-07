ae78752793fdb8ceb6171c3db261f4edf54bed9b
oukouwh <oukouwh@163.com>
2020-05-07 18:23:19 +0900

    测试一下

---
+++ Interpreter/data_type.py
@@ -0,0 +1,54 @@
+# 特别基础的东西，这里就简单写一下。加深自己印象。
+# 算术计算
+print(1+2)
+print(2*7)
+print(8/2)
+print(2**3)
+# 3
+# 14
+# 4.0
+# 8
+# 数据类型
+print(type(9))
+print(type(9.8))
+print(type('hello'))
+print(type([1,2,3,4]))
+print(type((1,2,3,4)))
+print(type({'A':1,'B':2}))
+# <class 'int'>
+# <class 'float'>
+# <class 'str'>
+# <class 'list'>
+# <class 'tuple'>
+# <class 'dict'>
+# 变量
+a = 10
+b = 2
+print(a*b)
+print(a+b)
+print(a-b)
+print(a/b)
+# 20
+# 12
+# 8
+# 5.0
+# 列表List
+a = [1,2,3,4,5,6,7]
+print(len(a))
+print(a)
+print(a[3])
+print(a[0:3])
+print(a[:4:2])
+print(a[1:])
+print(a[:-1])
+print(a[-1])
+# 7
+# [1, 2, 3, 4, 5, 6, 7]
+# 4
+# [1, 2, 3]
+# [1, 3]
+# [2, 3, 4, 5, 6, 7]
+# [1, 2, 3, 4, 5, 6]
+# 7
+# 字典dict
+
